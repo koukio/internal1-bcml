99cf4a0cc18e54be35b1b27e130028eb27f703f8
Juan Carlos Coll <juancarlos.coll@koukio.com>
2017-04-06 13:18:21 +0200

    Created DvdServiceInterface

---
+++ interal1-bcml-service/src/main/java/com/koukio/service/DvdServiceInterface.java
@@ -0,0 +1,22 @@
+package com.koukio.service;
+
+import java.util.Date;
+import java.util.List;
+
+import com.koukio.entity.Dvd;
+
+public interface DvdServiceInterface {
+    public Dvd createDvd(String title, String description, String category, Date dateCreated) throws Exception;
+    
+    public Dvd updateDvd(int dvdId, String title, String description, String category, Date dateCreated) throws Exception;
+    
+    public Dvd deleteDvd(int dvdId) throws Exception;
+    
+    public List<Dvd> showDvdList() throws Exception;
+    
+    public List<Dvd> searchDvdTitle() throws Exception;
+    
+    public List<Dvd> searchDvdKeyword() throws Exception;
+    
+    public String ping();
+}
