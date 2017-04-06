9781fb4e0606ad7e4ad84b4023f45eaf89ca916b
Juan Carlos Coll <juancarlos.coll@koukio.com>
2017-04-06 10:11:29 +0200

    Dvd entity created

---
+++ interal1-bcml-service/src/main/java/com/koukio/entity/Dvd.java
@@ -0,0 +1,42 @@
+package com.koukio.entity;
+
+import java.util.Date;
+
+public class Dvd {
+	public int dvdId;
+	public String title;
+	public String description;
+	public String category;
+	public Date dateCreated;
+	
+	public int getDvdId() {
+		return dvdId;
+	}
+	public void setDvdId(int dvdId) {
+		this.dvdId = dvdId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+}
