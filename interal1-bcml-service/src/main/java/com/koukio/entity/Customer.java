9e982b4c2d5f1f4b4bd5a3bcb023e6c0a6f7d559
Juan Carlos Coll <juancarlos.coll@koukio.com>
2017-04-06 13:40:37 +0200

    #4 - add email property

--- interal1-bcml-service/src/main/java/com/koukio/entity/Customer.java
+++ interal1-bcml-service/src/main/java/com/koukio/entity/Customer.java
@@ -12,13 +12,15 @@ public class Customer {
 	String lastName;
 	Date birthDate;
 	int customerId;
+	String email;
 	
-	public Customer( int customerId, String name, String fisrtName, String lastName, Date birthDate) {
+	public Customer( int customerId, String name, String fisrtName, String lastName, Date birthDate, String email) {
 		this.customerId = customerId;
 		this.name = name;
 		this.fisrtName = fisrtName;
 		this.lastName = lastName;
 		this.birthDate = birthDate;
+		this.email = email;
 	}
 
 	public String getName() {
@@ -51,6 +53,22 @@ public class Customer {
 
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	
 }
