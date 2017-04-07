61351c6f3dec5ea07722cc091653839c7637785c
joan fornes <joan.fornes@koukio.com>
2017-04-07 11:39:13 +0200

    #4 modify customerService

--- interal1-bcml-service/src/main/java/com/koukio/service/CustomerService.java
+++ interal1-bcml-service/src/main/java/com/koukio/service/CustomerService.java
@@ -2,7 +2,10 @@ package com.koukio.service;
 
 
 import com.koukio.entity.Customer;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.TreeSet;
 
 import org.springframework.stereotype.Component;
@@ -14,27 +17,25 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 @Component
 public class CustomerService {
 
-	TreeSet<Customer> customers = new TreeSet<>();
+	List<Customer> customers = new ArrayList<>();
 			
 	public String ping() {
 		return String.valueOf(System.currentTimeMillis());
 	}
 
-	public String createCustomer(String name, String firstName, String lastName, Date birthDate, String email) throws Exception{
+	public Customer createCustomer(String name, String firstName, String lastName, Date birthDate, String email) throws Exception{
 		//        throw new NotImplementedException();		
 		int customerId=customers.size();
 		Customer customer = new Customer(customerId, name, firstName, lastName, birthDate, email);
-		if (customers.add(customer)) {
-			return "Customer Created";
-		}else return "This customer already exists";
+		customers.add(customer);
+		return customer;
 	}
 	
-	public String updateCustomer(int customerId, String name, String firstName, String lastName, Date birthDate, String email) {
+	public Customer updateCustomer(int customerId, String name, String firstName, String lastName, Date birthDate, String email) {
 		//      throw new NotImplementedException();
 		Customer customer = new Customer(customerId, name, firstName, lastName, birthDate, email);
-		if (customers.add(customer)) {
-			return "Customer Update";
-		}else return "This customer already exists";
+		customers.set(customerId,customer);
+		return customer;
 	}
 	
 	
