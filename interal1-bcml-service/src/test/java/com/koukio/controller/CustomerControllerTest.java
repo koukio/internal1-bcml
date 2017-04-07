406e8a2cb399d9dcde2e4cc100a07d49f7e8de1f
Juan Carlos Coll <juancarlos.coll@koukio.com>
2017-04-07 12:36:44 +0200

    #5 Finish CustomerControllerTest

--- interal1-bcml-service/src/test/java/com/koukio/controller/CustomerControllerTest.java
+++ interal1-bcml-service/src/test/java/com/koukio/controller/CustomerControllerTest.java
@@ -1,5 +1,7 @@
 package com.koukio.controller;
 
+import java.util.Date;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,5 +22,33 @@ public class CustomerControllerTest {
     @Test
     public void pingTest() {
         Assert.assertNotNull(customerController.ping());
+    }
+    
+    @Test
+    public void ValidateCustomerOk() {
+    	Date date = new Date(07/11/1983);
+        Assert.assertTrue(customerController.validateCustomer("Sara Herreros", "Sara", "Herreros", date, "email@outlook.com"));
+    }
+    
+    @Test
+    public void ValidateCustomerEmptyField() {
+    	Date date = new Date(07/11/1983);
+        Assert.assertFalse(customerController.validateCustomer("Sara Herreros", "Sara", "", date, "email@outlook.com"));
+    }
+    
+    @Test
+    public void ValidateCustomerWrongDate() {
+    	Date date = new Date(07/11/2150);
+        Assert.assertFalse(customerController.validateCustomer("Sara Herreros", "Sara", "", date, "email@outlook.com"));
+    }
+    
+    @Test
+    public void ValidateEmailOk() {
+    	Assert.assertTrue(customerController.validate("email@outlook.com"));
+    }
+    
+    @Test
+    public void ValidateEmailWrong() {
+    	Assert.assertFalse(customerController.validate("emailoutlook.com"));
     }
 }
