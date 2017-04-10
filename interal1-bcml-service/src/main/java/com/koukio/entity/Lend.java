2a1bba80f72e09384d3767e99c711eb46ff6aae8
joan fornes <joan.fornes@koukio.com>
2017-04-10 11:04:19 +0200

    #8 generate equals

--- interal1-bcml-service/src/main/java/com/koukio/entity/Lend.java
+++ interal1-bcml-service/src/main/java/com/koukio/entity/Lend.java
@@ -31,6 +31,42 @@ public class Lend {
 	public void setTaken(Boolean taken) {
 		this.taken = taken;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+		result = prime * result + ((dvd == null) ? 0 : dvd.hashCode());
+		result = prime * result + ((taken == null) ? 0 : taken.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lend other = (Lend) obj;
+		if (customer == null) {
+			if (other.customer != null)
+				return false;
+		} else if (!customer.equals(other.customer))
+			return false;
+		if (dvd == null) {
+			if (other.dvd != null)
+				return false;
+		} else if (!dvd.equals(other.dvd))
+			return false;
+		if (taken == null) {
+			if (other.taken != null)
+				return false;
+		} else if (!taken.equals(other.taken))
+			return false;
+		return true;
+	}
 	
 }
