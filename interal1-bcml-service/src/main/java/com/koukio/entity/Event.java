a2e0f8041d0267e8470f92edc69bcec079ea727c
Juan Carlos Coll <juancarlos.coll@koukio.com>
2017-04-18 12:06:47 +0200

    Added eventID property

--- interal1-bcml-service/src/main/java/com/koukio/entity/Event.java
+++ interal1-bcml-service/src/main/java/com/koukio/entity/Event.java
@@ -4,9 +4,15 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Event {
+	
+	@Id
+	@GeneratedValue
+	private int eventId;
 	
 	@Column(nullable = false)
 	public String action;
