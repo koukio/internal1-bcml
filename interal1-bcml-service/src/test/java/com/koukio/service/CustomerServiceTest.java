5924f93937b4b773386de7e1cf453d14807e72a1
Ricardo RFG. Fernandez Garcia <ricardo.fernandez.garcia@de.abnamroclearing.com>
2017-04-21 13:05:56 +0200

    #15 - Configure test database

--- interal1-bcml-service/src/test/java/com/koukio/service/CustomerServiceTest.java
+++ interal1-bcml-service/src/test/java/com/koukio/service/CustomerServiceTest.java
@@ -1,16 +1,19 @@
 package com.koukio.service;
 
-import java.util.Date;
+import com.koukio.entity.Customer;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import com.koukio.entity.Customer;
+
+import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations="classpath:application-test.properties")
 public class CustomerServiceTest {
 
     @Autowired
--- interal1-bcml-service/src/test/java/com/koukio/service/EventServiceTest.java
+++ interal1-bcml-service/src/test/java/com/koukio/service/EventServiceTest.java
@@ -1,20 +1,21 @@
 package com.koukio.service;
 
-import java.util.Date;
-import java.util.List;
-
+import com.koukio.entity.Event;
+import com.koukio.repository.EventRepository;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.koukio.entity.Event;
-import com.koukio.repository.EventRepository;
+import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations="classpath:application-test.properties")
 public class EventServiceTest {
 
 	@Autowired
