831febe43397bb299a0a41ce5097bb780df3f78a
Juan Carlos Coll <juancarlos.coll@koukio.com>
2017-04-21 13:22:02 +0200

    #5 Fix CustomerServiceTest error

--- interal1-bcml-service/src/test/java/com/koukio/service/CustomerServiceTest.java
+++ interal1-bcml-service/src/test/java/com/koukio/service/CustomerServiceTest.java
@@ -1,6 +1,8 @@
 package com.koukio.service;
 
 import com.koukio.entity.Customer;
+import com.koukio.repository.CustomerRepository;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,6 +21,9 @@ public class CustomerServiceTest {
     @Autowired
     CustomerService customerService;
     
+    @Autowired
+    CustomerRepository customerRepository;
+    
     @Test
     public void createCustomerNotNull() throws Exception {
     	Date date = new Date(16/02/1964);
@@ -28,7 +33,7 @@ public class CustomerServiceTest {
     @Test
     public void createCustomerTest() throws Exception {
     	Date date = new Date(16/02/1964);
-    	Customer customer = new Customer(0,"Alberto Perez", "Alberto", "Perez", date, "aperez@gmail.com");
+    	Customer customer = new Customer((int) customerRepository.count()+1,"Alberto Perez", "Alberto", "Perez", date, "aperez@gmail.com");
         Assert.assertEquals(customer, customerService.createCustomer("Alberto Perez", "Alberto", "Perez", date, "aperez@gmail.com"));
     }
     
