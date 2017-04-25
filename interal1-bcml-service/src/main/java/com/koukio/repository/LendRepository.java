107034ca3b1f6cbaf7ddd7ff691468a002b25bc1
joan fornes <joan.fornes@koukio.com>
2017-04-25 12:27:03 +0200

    repository lend

--- interal1-bcml-service/src/main/java/com/koukio/repository/LendRepository.java
+++ interal1-bcml-service/src/main/java/com/koukio/repository/LendRepository.java
@@ -1,17 +1,9 @@
 package com.koukio.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.repository.CrudRepository;
-import com.koukio.entity.Customer;
 import com.koukio.entity.Lend;
 
 public interface LendRepository extends CrudRepository<Lend, Integer> {
-
-	Page<Lend> findAll(Pageable pageable);
-
-	Lend findByCustomerAllIgnoringCase(Customer customer);
-	int countByCustomerAndTakenAllIgnoringCase(Customer customer, boolean taken);
-	Lend findByCustomerAndTakenAllIgnoringCase(Customer customer, boolean taken);
 
 }
