708ea15be0b027258e58c411d3e1c533bf0bc03d
Juan Carlos Coll <juancarlos.coll@koukio.com>
2017-04-26 10:43:19 +0200

    #4 #10 Added create event to customer methods

--- interal1-bcml-service/src/main/java/com/koukio/service/CustomerService.java
+++ interal1-bcml-service/src/main/java/com/koukio/service/CustomerService.java
@@ -1,8 +1,9 @@
 package com.koukio.service;
 
-
 import com.koukio.entity.Customer;
+import com.koukio.entity.Event;
 import com.koukio.repository.CustomerRepository;
+import com.koukio.repository.EventRepository;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,9 @@ public class CustomerService {
 	@Autowired
 	CustomerRepository customerRepository;
 	
+	@Autowired
+	EventRepository eventRepository;
+	
 	public String ping() {
 		return String.valueOf(System.currentTimeMillis());
 	}
@@ -24,12 +28,18 @@ public class CustomerService {
 	public Customer createCustomer(String name, String firstName, String lastName, Date birthDate, String email) throws Exception{	
 		Customer customer = new Customer(name, firstName, lastName, birthDate, email);
 		customerRepository.save(customer);
+        Date date = new Date();
+        Event event = new Event("Create Customer", String.valueOf(customer.getCustomerId()), date);
+        eventRepository.save(event);
 		return customer;
 	}
 	
 	public Customer updateCustomer(String name, String firstName, String lastName, Date birthDate, String email) {
 		Customer customer = new Customer(name, firstName, lastName, birthDate, email);
 		customerRepository.save(customer);
+        Date date = new Date();
+        Event event = new Event("Update Customer", String.valueOf(customer.getCustomerId()), date);
+        eventRepository.save(event);
 		return customer;
 	}
 
